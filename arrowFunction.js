fca9880a9f0d015276a1257bbd48956b277a3fcc
Lucas Veras <140025978+lverasl@users.noreply.github.com>
2023-10-24 16:05:14 -0300

    Initial commit

---
+++ arrowFunction.js
@@ -0,0 +1,20 @@
+function apresentar(nome) {
+    return `meu nome é ${nome}`;
+}
+
+// Hoisting: Arrow Function se comparta como expressão
+//Operador >= (maior ou igual que)
+// Arrow Function 
+
+const apresentaArrow = nome `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+//Arrow function com + de 1 linha de instrução
+
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1 > 10 || num2 > 10) {
+        return "somente numeros de 1 a 9"
+    } else {
+        return num1 + num2;
+    }
+}
